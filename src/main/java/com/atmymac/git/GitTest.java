5856c321f7d2869bb6440925bd2e21d47efb319c
ZouJiacheng <zjc19990707@qq.com>
2021-12-18 16:14:57 +0800

    idea_first_commit

---
+++ src/main/java/com/atmymac/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atmymac.git;
+
+/**
+ * @author zjc
+ * @create 2021-12-18-4:09 下午
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
