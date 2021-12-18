7a3f80cd43626f1906ee415a13e7f04430b41f61
ZouJiacheng <zjc19990707@qq.com>
2021-12-18 17:57:41 +0800

    gitee commit

--- src/main/java/com/atmymac/git/GitTest.java
+++ src/main/java/com/atmymac/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hello git5 master test");
         System.out.println("hello git5 hot fix test");
+        System.out.println("gitee git5 hot fix test");
     }
 }
